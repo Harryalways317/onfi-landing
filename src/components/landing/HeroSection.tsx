6c5eb432f0bab9f0946eba4da1c00716ba620653
Harish Vadaparty <harishvadaparty@gmail.com>
2024-10-21 13:34:47 +0530

    Update HeroSection.tsx

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
       <div className='flex flex-col items-center'>
         <div className='space-grotesk-bold text-[32px] leading-8 sm:text-[36px] sm:leading-[36px] md:text-[42px] md:leading-[42px] lg:text-[52px] lg:leading-[52px] mb-10 md:mb-[13px] custom-text-gradient text-center px-6'>Explainable AI Agents for financial services</div>
         <div className='text-base md:text-lg dm-sans-regular text-[#505050] mb-8 md:mb-[26px] px-10 text-center'>Get verifiable and controllable responses from LLMs on financial documents like regulatory circulars.</div>
-        <a href='https://www.loom.com/share/8c89b40783c8416a9bbf3cdd97719b9a?sid=90b47d7f-f2ea-457d-8d25-cd284dbf036a' target="_blank" className='btn-gradient rounded-[4.5px] text-base text-white exo-2-bold px-[52px] py-3 md:py-4'>Get Free Trail</a>
+        <a href='https://www.loom.com/share/8c89b40783c8416a9bbf3cdd97719b9a?sid=90b47d7f-f2ea-457d-8d25-cd284dbf036a' target="_blank" className='btn-gradient rounded-[4.5px] text-base text-white exo-2-bold px-[52px] py-3 md:py-4'>Get Free Trial</a>
       </div>
       <div className='relative hidden md:block w-[99%] md:w-[80%] lg:w-[65%] h-fit mr-6'>
         <Image src={HeroImage} alt='hero-image' className='object-cover' />
