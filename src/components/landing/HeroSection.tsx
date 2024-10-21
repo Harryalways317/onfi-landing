affd43fabd4c2c92046e12fb2f142586ffdfaf94
Harish Vadaparty <harishvadaparty@gmail.com>
2024-10-21 14:17:34 +0530

    changes

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import HeroImage from '@/assets/landing/HeroImage.svg'
+import HERO2 from '@/assets/landing/HERO2.svg'
 
 const HeroSection = () => {
   return (
@@ -10,8 +11,8 @@ const HeroSection = () => {
         <div className='text-base md:text-lg dm-sans-regular text-[#505050] mb-8 md:mb-[26px] px-10 text-center'>Get verifiable and controllable responses from LLMs on financial documents like regulatory circulars.</div>
         <a href='https://www.loom.com/share/8c89b40783c8416a9bbf3cdd97719b9a?sid=90b47d7f-f2ea-457d-8d25-cd284dbf036a' target="_blank" className='btn-gradient rounded-[4.5px] text-base text-white exo-2-bold px-[52px] py-3 md:py-4'>Get Free Trial</a>
       </div>
-      <div className='relative hidden md:block w-[99%] md:w-[80%] lg:w-[65%] h-fit mr-6'>
-        <Image src={HeroImage} alt='hero-image' className='object-cover' />
+      <div className='relative hidden md:block w-[99%] md:w-[80%] lg:w-[65%] h-fit mt-8 md:mt-12 mr-6'>
+        <Image src={HERO2} alt='hero-image' className='object-cover' />
       </div>
     </div>
   )
